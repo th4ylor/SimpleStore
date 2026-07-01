1c5da961eece5b17fe75fc10e6cbcb2ed09155ae
isaak <isaak@casanova.imb.br>
2026-07-01 17:47:57 -0300

    estrutura princial

---
+++ scripts.js
@@ -0,0 +1,52 @@
+
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let itens = container.querySelectorAll('.list .item')
+let indicators = document.querySelector('.indicators')
+let dots = indicators.querySelectorAll('ul li')
+let list = container.querySelector('.list')
+
+let active = 0
+let firstPosition = 0
+let lastPosition = itens.length - 1
+
+function setSlider(){
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+     let dotsOld = indicators.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+
+    indicators.querySelector('.number').innerHTML = '0' + (active + 1)
+}
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+
+    if (active + 1 > lastPosition) {
+        active = 0
+    }
+    else{
+        active = active + 1
+    }
+    setSlider()
+    itens[active].classList.add('active')
+
+}
+
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+
+    if (active - 1 < firstPosition) {
+        active = lastPosition
+    }
+    else{
+        active = active - 1
+    }
+    setSlider()
+    itens[active].classList.add('active')
+
+}

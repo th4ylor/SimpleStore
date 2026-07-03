b36864173a9b86e09a00d6148a4f5b3ba615d60c
isaak <isaak@casanova.imb.br>
2026-07-03 14:14:06 -0300

    estilizando base da aba About

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 
-let prevButton = document.getElementById('prev')
-let nextButton = document.getElementById('next')
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
 let container = document.querySelector('.container')
 let itens = container.querySelectorAll('.list .item')
 let indicators = document.querySelector('.indicators')
 let dots = indicators.querySelectorAll('ul li')
 let list = container.querySelector('.list')
 let section = document.querySelector('section')
+let button1 = document.getElementById('saiba1')
+const button2 = document.querySelector('saiba2')
+const button3 = document.querySelector('saiba3')
 
 let active = 0
 let firstPosition = 0
@@ -24,7 +27,7 @@ function setSlider(){
     dots[active].classList.add('active')
 
     indicators.querySelector('.number').innerHTML = '0' + (active + 1)
-}
+};
 
 nextButton.onclick = () => {
     list.style.setProperty('--calculation', 1)
@@ -37,9 +40,7 @@ nextButton.onclick = () => {
     }
     setSlider()
     itens[active].classList.add('active')
-
-
-}
+};
 
 
 prevButton.onclick = () => {
@@ -53,6 +54,8 @@ prevButton.onclick = () => {
     }
     setSlider()
     itens[active].classList.add('active')
-  
+};
 
-}
+button1.addEventListener("click", () => {
+    window.location.href = "./pages/saibaferrari.html"
+});
